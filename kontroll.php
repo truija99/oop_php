f4df8190c8b992a7b7186221797c01c0b02c1410
Truija99 <Suema1234>
2018-12-11 09:25:52 +0200

    Ülesanne 1 - kirjeldus

---
+++ kontroll.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mari-liis.truija
+ * Date: 11.12.2018
+ * Time: 9:22
+ */
+
+//Ülesanne 1
+/*Loo prgogramm, mille sees:
+a) defineeritud muutujate abil
+    Sinu ees -ja perekonnanimi
+    Kursuse tähis
+    Kursuse number
+    Sinu kooli email
+b) Väljastatud muutujate väärtused koos sobiva tekstiga hmtl faili kujul
+*/
