607f421736874dba2cf3356c753c818f04ffe79b
Truija99 <Suema1234>
2018-12-11 11:43:29 +0200

    Värvilise tabeli loomine

--- kontroll.php
+++ kontroll.php
@@ -5,7 +5,8 @@
  * Date: 11.12.2018
  * Time: 9:22
  */
-echo '<html><head><title>Ülesanded</title></head><body>';
+echo '<html><head><title>Ülesanded</title><link rel="stylesheet" type="text/css" href="katsestyle.css"></head><body>';
+
 // Ülesanne 1
 /* Loo programm, mille sees:
 a) defineeritud muutujate abil
@@ -15,6 +16,7 @@ a) defineeritud muutujate abil
     Sinu kooli email
 b) Väljastatud muutujate väärtused koos sobiva tekstiga html faili kujul
 */
+
 echo '<h4>Ülesanne 1</h4>';
 $eesNimi = 'Mari-Liis';
 $pereNimi = 'Truija';
@@ -28,12 +30,15 @@ echo 'Kursuse tähis:'.$kursuseTahis.'<br>';
 echo 'Email:'.$email.'<br>';
 echo '<hr>';
 
+
+
 // Ülesanne 2
 /* Loo muutuja nimega varv, mille algväärtuseks pane 'punane';
 Koosta if-else if- else lause, mis väljastab teksti "Värviline tekst"
 vastavalt määratud väärtusega. Katseta vähemalt kolme HTML värviga -
 red, blue, orange - vastavalt tingimuslauses määra punane, sinine ja orange
 */
+
 echo '<h4>Ülesanne 2</h4>';
 $varv = 'orange';
 echo '<p style="color: ';
@@ -47,4 +52,26 @@ if($varv == 'punane'){
     echo 'black';
 }
 echo ';">Värviline tekst</p>';
+echo '<hr>';
+
+
+
+
+//Ülesanne 3 - gradient tabel umbes nagu katse.php-s
+
+echo '<h4>Ülesanne 3</h4>';
+echo '<table>';
+$ridadeArv = 6;
+$veergudeArv = 5;
+for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++){
+    echo '<tr>';
+    for($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++) {  //# - algab värvi määrang, ilma selleta ei tule tööle
+        $varv = '#'.$reaNumber.$veeruNumber.$reaNumber.$veeruNumber.$reaNumber.$veeruNumber; //3 vahelduvat värvi
+        echo '<td style="background-color: '.$varv.'">';
+        echo $veeruNumber;
+        echo '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
 echo '</body></html>';
