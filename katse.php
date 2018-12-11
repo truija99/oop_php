ab7a1a66f3814d5e17e182d895a050b60ab3ffa3
Truija99 <Suema1234>
2018-12-11 09:22:11 +0200

    Muutuja katsed

--- katse.php
+++ katse.php
@@ -6,17 +6,60 @@
  * Time: 11:08
  */
 
+//muutujate defineerimine
+//$muutujaNimi = väärtus;
+$lehePealkiri = 'Katse leht';
+$sisuPealkiri = 'Muutujate defineerimine';
+
+//muutujate sisu testvaatamine
+var_dump($lehePealkiri); //funktsioon
+echo '<br>'; //eraldi reale
+var_dump($sisuPealkiri); //funktsioon
+
+//andmetüübid
+$taisarv = 7;
+$reaalarv = -2.6;
+$tekst = 'Tere PHP'; //TEKST ON JUTUMÄRKIDES ALATI
+$toevaartusTrue = true; //BOOLEAN
+$toevaartusFalse = false;  //BOOLEAN
+
+//väärtuse tüübi kontroll - gettype($muutujaNimi)
+//väärtuse tüübi teisendus - settype ($muutujaNimi, 'tüüp')
+
+//väljastamine
+//ühendamine 'tekst katkeb '.$muutujaNimi.' tekst jätkub'
 echo '
     <!doctype html> 
     <html> 
         <head>
-            <title></title>
+            <title>'.$lehePealkiri.'</title>
         </head>
         <body>
-            <h1>PHP abil loodud HTML</h1>
+            <h1>'.$sisuPealkiri.'</h1>
+            <h3>Andmetüübid</h3>
+            <ul>
+                <li>$taisarv = '.$taisarv.' - '.gettype($taisarv).'</li>
+                <li>$reaalarv = '.$reaalarv.' - '.gettype($reaalarv).'</li>
+                <li>$tekst = '.$tekst.' - '.gettype($taisarv).'</li>
+                <li>$toevaartusTrue = '.$toevaartusTrue.' - '.gettype($toevaartusTrue).'</li>
+                <li>$toevaartusFalse = '.$toevaartusFalse.' - '.gettype($toevaartusFalse).'</li>
+            </ul>
         </body>
 </head>
     </html>
 ';
 
+//tüübi teisendamise katse
+echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
+echo '<br>';
+settype($taisarv, 'string');
+
+echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
+echo '<br>';
+echo gettype($taisarv);
+echo '
+        <body>
+    </html>
+
+'
 ?>
