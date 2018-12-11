508f13f41c8ed1c2f03235477644da5b0c0fdef4
Truija99 <Suema1234>
2018-12-11 08:42:41 +0200

    Katsetati PHP koodi tööd serverile üleslaadimisega

---
+++ katse.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mari-liis.truija
+ * Date: 06.12.2018
+ * Time: 11:08
+ */
+
+echo '
+    <!doctype html> 
+    <html> 
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <h1>PHP abil loodud HTML</h1>
+        </body>
+</head>
+    </html>
+';
+
+?>
