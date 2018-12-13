d1a00f31546ee9aff3d7c525549cd26ebff9b898
Truija99 <Suema1234>
2018-12-13 09:37:15 +0200

    Massiivid - numbrite massiivi loomine

--- katse.php
+++ katse.php
@@ -1,60 +1,60 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: mari-liis.truija
+ * User: anna.karutina
  * Date: 06.12.2018
  * Time: 11:08
  */
-
 // massiivid
 // array()
 /*
 $massiiv = array(); // tühi massiiv - ei ole sees midagi
-$massiiv [] = väärtus
-
+$massiiv[] = väärtus;
  */
 
-$nimed = array();
-$nimed[] = 'Mari-Liis';
-$nimed[] = 'Elen';
-var_dump($nimed); //väljastab selle ülemise asja seal
+//massiivi loomine
+$numbrid = array(
+    array(1, 2, 3),
+    array(4, 5, 6),
+    array(7, 8, 9)
+);
+
+//massiivi sisu test kontroll
+var_dump($numbrid);
 echo '<pre>';
-print_r($nimed);
-echo '/<pre>';
+print_r($numbrid);
+echo '</pre>';
 
+// massiivi elemendi väljatamine
+echo $numbrid[1][1];
 
-function htmlTable($ridadeArv, $veergudeArv){
+function htmlTabel($ridadeArv = 0, $veergudeArv = 0){
     $tabel = '<table>';
     for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++){
         $tabel = $tabel.'<tr>';
-            for($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++) {
-                $tabel = $tabel.'<td>';
-                $tabel = $tabel.$veeruNumber;
-                $tabel = $tabel.'</td>';
-            }
-        $tabel = $tabel.'</tr>';
+        for($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++) {
+            $tabel = $tabel.'<td>';
+            $tabel = $tabel.$veeruNumber;
+            $tabel = $tabel.'</td>';
         }
-     $tabel = $tabel.'</table>';
-     return $tabel; //tagastatakse tabel, veel ei väljastata (ainult echo kaudu)
+        $tabel = $tabel.'</tr>';
+    }
+    $tabel = $tabel.'</table>';
+    return $tabel;
 }
 
-//lehe sisu väljastamine
+// lehe sisu väljastamine
 echo '<!doctype html><html><head>
 <title>Funktsioonid</title>
 <link rel="stylesheet" type="text/css" href="katsestyle.css">
 </head><body>';
-
-//kutsume funktsiooni tööle
-$tabel1 = htmlTable(4, 4);
+// kutsume funktsioon tööle
+$tabel1 = htmlTabel(4, 4);
 echo $tabel1;
 echo '<hr>';
-
-$tabel2 = htmlTable(2, 4);
+$tabel2 = htmlTabel(2, 5);
 echo $tabel2;
 echo '<hr>';
-$tabel3 = htmlTable(1, 1);
-echo $tabel3;
-echo '<hr>';
+echo htmlTabel(1, 3);
 echo '</body></html>';
 ?>
-
