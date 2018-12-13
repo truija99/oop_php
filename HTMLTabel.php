c6b0ad325cffb58159efe4a43fa19051510e3625
Truija99 <Suema1234>
2018-12-13 11:36:21 +0200

    Klassid ja OOP - HTMLTabel klass

--- HTMLTabel.php
+++ HTMLTabel.php
@@ -11,4 +11,38 @@ class HTMLTabel extends Tabel
 {
 
 
+    /**
+     * HTMLTabel constructor.
+     */
+    public function __construct()
+    {
+        echo 'HTML';
+        parent:: __construct();
+    }
+
+    /**
+     * vaata tabel massiiv foreach abil ridade kaupa
+     * ja väljasta HTML tabeli kujul
+     */
+    function naitaTabel()
+    {
+        echo '<table style: border="1px solid black";>';
+        foreach ($this->tabel as $rida){        //tabel.php asi kättesaadav
+            echo '<tr>';
+            foreach ($rida as $element){ ;
+                echo '<td>' . $element . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+    }
 }
+
+
+
+
+
+
+
+
+
