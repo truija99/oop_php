24a4f2a5af8cd77ce7be5304a5b161b0bce18bbf
Truija99 <Suema1234>
2018-12-13 10:52:39 +0200

    Klassid ja OOP - Tabel klassi põhjal objekti loomine

---
+++ tabelTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mari-liis.truija
+ * Date: 13.12.2018
+ * Time: 10:44
+ */
+
+//Loen sisse Tabel.php faili sisu
+//pöördub faili mida hakkan nõudma ja paneb selle tööle, peab ainult ütlema faili asukoha
+require_once './Tabel.php'; //siinsamas kus ma asun, peab asuma see fail
+
+// loome tabeli objekti
+$lihtTabel = new Tabel();
+
+// lisame read sisuga - punk javas, siin noolemärk
+$lihtTabel->lisaRida(array(1, 2, 3));
+$lihtTabel->lisaRida(array(4, 5, 6));
+$lihtTabel->lisaRida(array(7, 8, 9));
+
+// väljastame tabeli
+$lihtTabel->naitaTabel();
