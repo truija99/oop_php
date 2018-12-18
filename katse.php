c2a45a8eb1bbef3405ab6de9667e8a6f98b7eb9a
Truija99 <Suema1234>
2018-12-18 09:10:33 +0200

    Massiivid - kahemõõtmeline sidestatud massiiv - nimed ja väärtused

--- katse.php
+++ katse.php
@@ -13,76 +13,42 @@ $massiiv[] = väärtus;
  */
 
 
-// massiivi loomine
-$numbrid = array(
-    array(1, 2, 3),
-    array(4, 5, 6)
-);
-// massiivi sisu test kontroll
-var_dump($numbrid);
-echo '<pre>';
-print_r($numbrid);
-echo '</pre>';
-
-// massiivi elemendi väljastamine
-echo $numbrid[1][1];
-echo '<br>';
-
-// massiivi suuruse kontroll - meil see on ridade arv
-$ridadeArv = count($numbrid);
-echo $ridadeArv;
-echo '<br>';
-
-// veergudeArv
-$veergudeArv = count($numbrid[0]);
-echo $veergudeArv;
-echo '<br>';
-
-// massiivi väljastamine for
-for($reaNumber = 0; $reaNumber < $ridadeArv; $reaNumber++){
-    for($veeruNumber = 0; $veeruNumber < $veergudeArv; $veeruNumber++){
-        echo $numbrid[$reaNumber][$veeruNumber].'&nbsp;';
-    }
-    echo '<br>';
-}
-
-// massiivi väljastamine foreach abil
-foreach ($numbrid as $rida){
-    foreach ($rida as $number);{
-        echo $number.'&nbsp';
-    }
-    echo  '<br>';
-}
-
-
-function htmlTabel($ridadeArv = 0, $veergudeArv = 0){
-    $tabel = '<table>';
-    for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++){
-        $tabel = $tabel.'<tr>';
-        for($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++) {
-            $tabel = $tabel.'<td>';
-            $tabel = $tabel.$veeruNumber;
-            $tabel = $tabel.'</td>';
+//koostame funktsiooni
+function valjastaInfo($massiiv) {
+    foreach ($massiiv as $alamMassiivNimi => $alamMassiivAndmed){
+        echo '<h5>'.$alamMassiivNimi.'</h5><br>';
+        foreach ($alamMassiivAndmed as $elemendiNimi => $elemendiVaartus){
+            echo $elemendiNimi. ' - '.$elemendiVaartus.'<br>';
         }
-        $tabel = $tabel.'</tr>';
+        echo '<hr>';
     }
-    $tabel = $tabel.'</table>';
-    return $tabel;
 }
+
+// massiivi loomine
+$perekondPeppa = array(
+    'Peppa' => array(
+        'nimi' => 'Peppa',
+        'amet' => 'põrsaslaps',
+        'vanus' => 5,
+        'sugu' => 'naine'
+    ),
+    'Georg' => array(
+        'nimi' => 'Georg',
+        'amet' => 'põrsaslaps',
+        'vanus' => 2,
+        'sugu' => 'mees'
+    )
+);
+
+
 
 // lehe sisu väljastamine
 echo '<!doctype html><html><head>
-<title>Funktsioonid</title>
+<title>Massiivid</title>
 <link rel="stylesheet" type="text/css" href="katsestyle.css">
 </head><body>';
 
-// kutsume funktsioon tööle
-$tabel1 = htmlTabel(4, 4);
-echo $tabel1;
-echo '<hr>';
-$tabel2 = htmlTabel(2, 5);
-echo $tabel2;
-echo '<hr>';
-echo htmlTabel(1, 3);
+valjastaInfo($perekondPeppa);
+
 echo '</body></html>';
 ?>
